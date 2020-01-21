84105c7bc8acd69268999659b1f852f8d0e9b3b1
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-21 15:37:30 +0100

    inizio lavoro

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+// *Simon says*
+// Un alert espone 5 numeri casuali.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+// Consigli del giorno:
+// Partite sempre dalla logica e poi passate a scrivere in js.
+// Usate le slide per vedere la sintassi delle nuove cose studiate :)
+// Pushate ogni piccolo passo in avanti!
+// Qualunque sperimentazione è benvenuta, ma come sempre mettetela in una cartella bonus lasciando l'esercizio base funzionante come index principale
